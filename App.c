06ce008466ada9ca9ac761a6e106b0b7e018bb42
bpas99 <belenarpas@gmail.com>
2022-12-18 19:33:05 +0100

    Ejercicio funcional

--- App.c
+++ App.c
@@ -1,26 +1,33 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    //Primero llamamos a esta funcion para reiniciar el calculo de numeros aleatorios
-    srand (time(NULL));
+int main(void)
+{
+    // Variables dadas en el ejercicio
+    int p = 761;
+    int r = 6;
+    // Primero llamamos a esta funcion para reiniciar el calculo de numeros aleatorios
+    srand(time(NULL));
     // Creamos un numero x aleatorio
-    int x = rand();
-    int y;
-    int k = 0;
+    int e = (rand() % 5000 + 1);
+    int y = 0;
 
-	printf("Send the number X: %d to your partner.\n",x);
+    int x = mod_exp(r, e, p);
+
+    printf("Send the number X: %d to your partner.\n", x);
     printf("Enter the Y number that your partner has sent you:");
-    scanf(" %d",&y);
-	printf("The shared secret key is:: %d.\n",k);
+    scanf(" %d", &y);
+    int k = mod_exp(y, e, p);
+    printf("The shared secret key is:: %d.\n", k);
 
-	return(0); 
+    return (0);
 }
 
-int mod_exp(int b, int e, int p)
+int mod_exp(int r, int e, int p)
 {
     int i, x, power;
     x = 1;
-    power = b % p;
+    power = r % p;
     for (i = 0; i < 8 * sizeof(int); i++)
     {
         if (e & 1)
@@ -30,5 +37,6 @@ int mod_exp(int b, int e, int p)
         e >>= 1;
         power = (power * power) % p;
     }
+
     return x;
 }
